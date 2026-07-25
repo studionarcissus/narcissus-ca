5953fc4da718f604aba873a7bc697677a648b877
studionarcissus <studionarcissus@outlook.com>
2026-07-25 12:29:31 -0700

    Production v1.0 - Luxury redesign

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const year = document.getElementById('year');
+
+    if (year) {
+        year.textContent = new Date().getFullYear();
+    }
+});
